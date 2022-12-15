446f22492d3553562e3e0d48a26a5ac86c549d4a
Sn1kerS20 <118538505+Sn1kerS20@users.noreply.github.com>
2022-12-15 14:54:37 +0200

    Добавьте файлы проекта.

---
+++ calculators.py
@@ -0,0 +1,40 @@
+def sum(a, b):
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    result = a + b
+    print("Result is: ", result)
+
+sum(0,0)
+
+def riz(a, b):
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    result = a - b
+    print("Result is: ", result)
+
+riz(0,0)
+
+def dob(a, b):
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    result = a * b
+    print("Result is: ", result)
+
+dob(0,0)
+
+def chas(a, b):
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    result = a / b
+    print("Result is: ", result)
+
+chas(0,0)
+
+def kvr(a):
+    a = int(input("Enter a:"))
+    result = a**2
+    print("Result is: ", result)
+
+kvr(0)
+
+
